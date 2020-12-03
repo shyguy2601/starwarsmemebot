33729fce508f2b0e68e5c4ca6689414f77c1d399
hayden <07htravers@mybest.org.uk>
2020-12-03 20:57:01 +0000

    Update index.js

--- index.js
+++ index.js
@@ -43,9 +43,6 @@ Client.once('ready', () => {
       
         if (message.content == `<@!783629275546648577> prefix`)
           Client.commands.get(`prefix`).execute(message, args);
-      
-        const command = Client.commands.get(commandName) || Client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
-      
         if (message.channel.id === '784015463747026959') {
           message.attachments.forEach(attachment => {
             const userMemeEmbed = new Discord.MessageEmbed()
@@ -60,9 +57,14 @@ Client.once('ready', () => {
           })
           
     const command = Client.commands.get(commandName) || Client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));  
-      command.execute(message, args).catch(command);
-
-        }
+    if(command == 'entermeme'){
+        Client.commands.get(`entermeme`).execute(message, args);
+    } else if(command == 'starwarsmeme' || command == 'swmeme'){
+    Client.commands.get('starwarsmeme').execute(message, args)
+    } else if(command == 'meme'){
+        Client.commands.get('meme').execute(message, args)
+    }
+}
       });
 
 Client.login(process.env.DJS_TOKEN);
