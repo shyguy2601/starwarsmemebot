6bdb732ab0f94ecb4dfe3f2124be87052aac91a8
hayden <07htravers@mybest.org.uk>
2020-12-03 22:35:40 +0000

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -11,8 +11,8 @@ module.exports = {
 	    .setTitle('Available Commands')
 	    .setAuthor('Help')
 	    .addFields(
-            { name: '>meme', value: 'Get a random meme from reddit' },
-            { name: '>starwarsmeme / >swmeme', value: 'Get a starwarsmeme from reddit' },
+            { name: '>meme', value: 'Get a random meme from reddit', inline: true },
+            { name: '>starwarsmeme / >swmeme', value: 'Get a starwarsmeme from reddit', inline: true },
 		    { name: '\u200B', value: '\u200B' },
 		    
 	    )
