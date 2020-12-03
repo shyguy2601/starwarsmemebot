10463d8cddf49d0a359fc630948a504ddb4d260e
hayden <07htravers@mybest.org.uk>
2020-12-03 15:06:50 +0000

    Update index.js

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ Client.on('message', message => {
     .setTitle(`Meme Entry by ${message.author.username}`)
     .setImage(attachment.url)
     .setDescription(`If you like the meme then react with 👍 or if you don't like it, react with 👎`)
-    message.guild.channels.cache.find(i => i.name === 784020687013675038).send(userMemeEmbed)
+    message.guild.channels.cache.find(i => i.id === 784020687013675038).send(userMemeEmbed)
     .then(embed => {
         embed.react('👍')
         .then(() => embed.react('👎'));
