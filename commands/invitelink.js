c4eb6137cd75cf1c437e38f6df683c0df6bccecb
hayden <07htravers@mybest.org.uk>
2020-12-03 17:58:44 +0000

    Update invitelink.js

--- commands/invitelink.js
+++ commands/invitelink.js
@@ -5,6 +5,6 @@ module.exports = {
       const Discord = require('discord.js');
       const prefix = ">";
 
-  message.channel.send(`https://discord.com/oauth2/authorize?client_id=783629275546648577&scope=bot&permissions=2085088497`)
+  message.channel.send(`https://discord.com/oauth2/authorize?client_id=783629275546648577&scope=bot&permissions=1883630704`)
   }
 }
