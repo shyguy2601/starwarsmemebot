7a56137c12d75beb3d0b6bc1764cc2a5d572cc07
hayden <07htravers@mybest.org.uk>
2020-12-03 19:57:11 +0000

    Update index.js

--- index.js
+++ index.js
@@ -41,8 +41,12 @@ Client.once('ready', () => {
       
         const commandName = args[0].slice(prefix.length).toLowerCase();
       
-        if (message.content == `<@!783629275546648577> prefix`)
+        if(message.content == `<@!783629275546648577> prefix`){
           Client.commands.get(`prefix`).execute(message, args);
+        }
+        if(message.content == "pls meme"){
+            message.reply('pls meme is disabled, pls use >meme instead ')
+        }
       
         const command = Client.commands.get(commandName) || Client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
       
