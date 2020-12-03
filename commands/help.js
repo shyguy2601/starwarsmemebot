bf78209fb3c29e0106b6338ad97c658b4b02dc15
hayden <07htravers@mybest.org.uk>
2020-12-03 22:21:06 +0000

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -2,9 +2,28 @@ module.exports = {
     name: 'prefix',
     description: "this is a command to remind users of prefix",
     execute(message, args){
-      const Discord = require('discord.js');
-      const prefix = ">";
+        const Discord = require('discord.js');
+        const prefix = ">";
+        const { MessageEmbed } = require('discord.js');
+        
+        const helpEmbed = new Discord.MessageEmbed
+        .setColor('#0099ff')
+	    .setTitle('Some title')
+	    .setURL('https://discord.js.org/')
+	    .setAuthor('Some name', 'https://i.imgur.com/wSTFkRM.png', 'https://discord.js.org')
+	    .setDescription('Some description here')
+	    .setThumbnail('https://i.imgur.com/wSTFkRM.png')
+	    .addFields(
+		    { name: 'Regular field title', value: 'Some value here' },
+		    { name: '\u200B', value: '\u200B' },
+		    { name: 'Inline field title', value: 'Some value here', inline: true },
+		    { name: 'Inline field title', value: 'Some value here', inline: true },
+	    )
+	    .addField('Inline field title', 'Some value here', true)
+	    .setImage('https://i.imgur.com/wSTFkRM.png')
+	    .setTimestamp()
+	    .setFooter('Some footer text here', 'https://i.imgur.com/wSTFkRM.png');
 
-  message.channel.send(`My prefix is ${prefix}`)
+
   }
 }
