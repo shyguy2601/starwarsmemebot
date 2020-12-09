9dbaaa569e67e11cbfe5eaab5e84a183eb114f07
hayden <07htravers@mybest.org.uk>
2020-12-09 12:10:34 +0000

    Update hpmeme.js

--- commands/hpmeme.js
+++ commands/hpmeme.js
@@ -6,23 +6,66 @@ module.exports = {
     description: 'Sends harry potter meme.',
     aliases:['harrypottermeme', 'hpm'],
     execute(message, args) {
-            https.get('https://api.macedon.ga/reddit/random/subreddit?sub=harrypottermemes', (resp) => {
-                let data = '';
-                resp.on('data', (chunk) => {
-                    data += chunk;
+        if (message.channel.type == "text") {
+            function getMeme() {
+                return new Promise((resolve, reject) => {
+                    https.get('https://api.macedon.ga/reddit/random/subreddit?sub=harrypottermemes', (resp) => {
+                        let data = '';
+                        resp.on('data', (chunk) => {
+                            data += chunk;
+                        });
+                        resp.on('end', () => {
+                            var data = JSON.parse(data);
+                            if (data.image.includes("https://i.redd.it/"))
+                                resolve(data);
+                            else
+                                getMeme().then(data => resolve);
+                        });
+                    });
                 });
-                resp.on('end', () => {
-                    var res = JSON.parse(data);
-                    const embed = new Discord.MessageEmbed()
-                        //.setColor(message.member.displayColor)
-                        .setTitle(res.title)
-                        .setURL(res.permalink)
-                        .setImage(res.image)
-                        .setTimestamp()
-                        .setFooter(`Requested by ${message.author.username}`)
-                    message.channel.send(embed);
-                });
+            }
+        
+            getMeme().then(data => {
+                const embed = new Discord.MessageEmbed()
+                    .setColor(message.member.displayColor)
+                    .setTitle(data.title)
+                    .setURL(data.permalink)
+                    .setImage(data.image)
+                    .setTimestamp()
+                    .setFooter(`Requested by ${message.author.username}`)
+                message.channel.send(embed);
             });
+        }if (message.channel.type == "text") {
+            function getMeme() {
+                return new Promise((resolve, reject) => {
+                    https.get('https://api.macedon.ga/reddit/random/subreddit?sub=harrypottermemes', (resp) => {
+                        let data = '';
+                        resp.on('data', (chunk) => {
+                            data += chunk;
+                        });
+                        resp.on('end', () => {
+                            var data = JSON.parse(data);
+                            if (data.image.includes("https://i.redd.it/"))
+                                resolve(data);
+                            else
+                                getMeme().then(data => resolve);
+                        });
+                    });
+                });
+            }
+        
+            getMeme().then(data => {
+                const embed = new Discord.MessageEmbed()
+                    .setColor(message.member.displayColor)
+                    .setTitle(data.title)
+                    .setURL(data.permalink)
+                    .setImage(data.image)
+                    .setTimestamp()
+                    .setFooter(`Requested by ${message.author.username}`)
+                message.channel.send(embed);
+            });
+        }
+          
        
     },
 };
