86dbf005b6a99d1b070a67c77bca8f5275982c1b
hayden <07htravers@mybest.org.uk>
2020-12-09 13:08:30 +0000

    Added >insult to help embed

--- commands/help.js
+++ commands/help.js
@@ -16,7 +16,9 @@ module.exports = {
             { name: '>meme / >m', value: 'Get a random meme from reddit', inline: true },
             { name: '>starwarsmeme / >swmeme / >swm', value: 'Get a starwars meme from reddit', inline: true },
             { name: '>harrypottermeme / >hpmeme / >hpm', value: 'Get a harry potter mmeme from reddit', inline: true },
-		    { name: '\u200B', value: '\u200B' },
+            { name: '\u200B', value: '\u200B' },
+            { name: 'Fun Related Commands', value: '\u200B' },
+            { name: `>insult [ping the user you want to insult]`, value: `Insults the mentioned user`, inline: true},
 		    
 	    )
 	    .setTimestamp()
