3739363bd0cc96ecd0dcd2d9002ad92a0590359b
hayden <07htravers@mybest.org.uk>
2020-12-09 16:49:46 +0000

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -10,15 +10,15 @@ module.exports = {
 
       const helpEmbed = new Discord.MessageEmbed()
           .setTitle(`Help - \`\`${NextToCommand}\`\``)
-          .addFields(
-            { name: '__Available Commands:-__', value: '\u200B' }, { name: `__Meme Related Commands__`, value: '\u200B' }, { name: '>meme / >m', value: 'Get a random meme from reddit', inline: true }, { name: '>starwarsmeme / >swmeme / >swm', value: 'Get a starwars meme from reddit', inline: true }, { name: '>harrypottermeme / >hpmeme / >hpm', value: 'Get a harry potter meme from reddit', inline: true }, { name: '__Fun Related Commands__', value: '\u200B' }, { name: `${insultExample}`, value: `Insults the mentioned user`, inline: true }, )
+          .addFields({ name: '__Available Commands:-__', value: '\u200B' }, { name: `__Meme Related Commands__`, value: '\u200B' }, { name: '>meme / >m', value: 'Get a random meme from reddit', inline: true }, { name: '>starwarsmeme / >swmeme / >swm', value: 'Get a starwars meme from reddit', inline: true }, { name: '>harrypottermeme / >hpmeme / >hpm', value: 'Get a harry potter meme from reddit', inline: true })
           .setTimestamp()
           .setFooter(`Requested by ${message.author.username}`);
 
       if (message.guild.id = '784015462303399958')
           helpEmbed.addFields({ name: '>entermeme', value: 'Enter a meme into meme-entrys to try win the meme competition', inline: true })
 
+      helpEmbed.addFields({ name: '__Fun Related Commands__', value: '\u200B' }, { name: `${insultExample}`, value: `Insults the mentioned user`, inline: true });
+
       message.channel.send(helpEmbed);
   }
 }
-           
