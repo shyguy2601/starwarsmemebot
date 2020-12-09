d7a1e360af1004c1e34ab6169c17207292ee40cd
hayden <07htravers@mybest.org.uk>
2020-12-09 14:06:38 +0000

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -9,14 +9,14 @@ module.exports = {
         const NextToCommand = "[] next to the command name is a mandatory thing for the command to work"
         const helpEmbed = new Discord.MessageEmbed()
       .setTitle(`Help -`,"```" +`${NextToCommand} `+"```")
-      .addField({ name:'_Available Commands:-_', value: '\u200B'})
+      .addField({ name:'__Available Commands:-__', value: '\u200B'})
 	    .addFields(
-            { name: `_Meme Related Commands_`, value: '\u200B' },
+            { name: `__Meme Related Commands__`, value: '\u200B' },
             { name: '>meme / >m', value: 'Get a random meme from reddit', inline: true },
             { name: '>starwarsmeme / >swmeme / >swm', value: 'Get a starwars meme from reddit', inline: true },
             { name: '>harrypottermeme / >hpmeme / >hpm', value: 'Get a harry potter mmeme from reddit', inline: true },
             { name: '\u200B', value: '\u200B' },
-            { name: '_Fun Related Commands_', value: '\u200B' },
+            { name: '__Fun Related Commands__', value: '\u200B' },
             { name: `>insult [ping the user you want to insult]`, value: `Insults the mentioned user`, inline: true},
 		    
 	    )
