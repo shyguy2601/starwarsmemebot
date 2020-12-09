b3e1335a745f67f743b68429632bf2934f059d1b
hayden <07htravers@mybest.org.uk>
2020-12-09 17:57:04 +0000

    Update userinfo.js

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -17,41 +17,35 @@ const flags = {
 	VERIFIED_DEVELOPER: 'Verified Bot Developer'
 };
 
-module.exports = class extends cmd {
-
-    constructor(...args){
-        super(...args, {
+module.exports = {
     name: 'userinfo',
     description: "gets information of a user",
     aliases: ['ui', 'whois'],
-   });
-}
-
-async run(message, [target]){
+    execute: (message, args) => {
     const member = message.mentions.members.last() || message.guild.members.cache.get(target) || message.member;
     const roles = member.roles.cache    
         .sort((a, b) => b.position - a.position)
         .map(role => role.toString())
         .slice(0, -1);
     const userflags = member.user.flags.toArray();
-    const embed = newMessageEmbed()
+    const embed = new MessageEmbed()
     .setThumbnail(member.user.displayAvatarURL({ dynamic: true, size: 512}))
     .addField('User', [
-        `**❯ Username:** ${member.user.username}`,
-        `**❯ Discriminator:** ${member.user.discriminator}`,
-        `**❯ ID:** ${member.id}`,
-        `**❯ Flags:** ${userFlags.length ? userFlags.map(flag => flags[flag]).join(', ') : 'None'}`,
-        `**❯ Avatar:** [Link to avatar](${member.user.displayAvatarURL({ dynamic: true })})`,
-        `**❯ Time Created:** ${moment(member.user.createdTimestamp).format('LT')} ${moment(member.user.createdTimestamp).format('LL')} ${moment(member.user.createdTimestamp).fromNow()}`,
-        `**❯ Status:** ${member.user.presence.status}`,
-        `**❯ Game:** ${member.user.presence.game || 'Not playing a game.'}`,
+        `**> Username:** ${member.user.username}`,
+        `**> Discriminator:** ${member.user.discriminator}`,
+        `**> ID:** ${member.id}`,
+        `**> Flags:** ${userFlags.length ? userFlags.map(flag => flags[flag]).join(', ') : 'None'}`,
+        `**> Avatar:** [Link to avatar](${member.user.displayAvatarURL({ dynamic: true })})`,
+        `**> Time Created:** ${moment(member.user.createdTimestamp).format('LT')} ${moment(member.user.createdTimestamp).format('LL')} ${moment(member.user.createdTimestamp).fromNow()}`,
+        `**> Status:** ${member.user.presence.status}`,
+        `**> Game:** ${member.user.presence.game || 'Not playing a game.'}`,
         `\u200b`
     ])
     .addField('Member', [
-        `**❯ Highest Role:** ${member.roles.highest.id === message.guild.id ? 'None' : member.roles.highest.name}`,
-        `**❯ Server Join Date:** ${moment(member.joinedAt).format('LL LTS')}`,
-        `**❯ Hoist Role:** ${member.roles.hoist ? member.roles.hoist.name : 'None'}`,
-        `**❯ Roles [${roles.length}]:** ${roles.length < 10 ? roles.join(', ') : roles.length > 10 ? this.client.utils.trimArray(roles) : 'None'}`,
+        `**> Highest Role:** ${member.roles.highest.id === message.guild.id ? 'None' : member.roles.highest.name}`,
+        `**> Server Join Date:** ${moment(member.joinedAt).format('LL LTS')}`,
+        `**> Hoist Role:** ${member.roles.hoist ? member.roles.hoist.name : 'None'}`,
+        `**> Roles [${roles.length}]:** ${roles.length < 10 ? roles.join(', ') : roles.length > 10 ? this.client.utils.trimArray(roles) : 'None'}`,
         `\u200b`
     ]);
         return message.channel.send(embed);
