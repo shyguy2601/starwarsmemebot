530e6a9cb4f1c71f46408d3b19e4c69d15eb5ffd
hayden <07htravers@mybest.org.uk>
2020-12-10 21:38:35 +0000

    Update givelove.js

--- commands/givelove.js
+++ commands/givelove.js
@@ -5,7 +5,7 @@ module.exports = {
     execute(message, args){
       const Discord = require('discord.js');
       const prefix = ">";
-      const member = message.mentions.members.last() || message.guild.members.cache.get(args[0]) || message.member;
+      const member = message.member;
     const userToLove = message.mentions.members.first() || message.guild.members.get(args[1]);
   message.channel.send(`${member.user.username} kissed ${userToLove}`);
 
