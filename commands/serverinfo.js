ceab26e52492e411e6d80e7eeefc4362fee54f82
hayden <07htravers@mybest.org.uk>
2020-12-10 18:23:34 +0000

    Update serverinfo.js

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -76,7 +76,8 @@ module.exports = {
 				`**❯ Offline:** ${members.filter(member => member.presence.status === 'offline').size}`,
 				'\u200b'
 			])
-			.addField(`Roles [${roles.length - 1}]`, roles.length < 10 ? roles.join(', ') : roles.length > 10 ? this.client.utils.trimArray(roles) : 'None')
+            .addField(`**> Roles [${roles.length}]:** ${roles.length > 0 ? roles.join(', '): 'None'}`,)
+            
 			.setTimestamp();
 		message.channel.send(embed);
 	}
