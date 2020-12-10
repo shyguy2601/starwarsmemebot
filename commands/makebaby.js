b2d2d3b6bc54b629b04633a1e3236a34edf700af
hayden <07htravers@mybest.org.uk>
2020-12-10 21:42:21 +0000

    Create makebaby.js

---
+++ commands/makebaby.js
@@ -0,0 +1,13 @@
+module.exports = {
+    name: 'makebaby',
+    description: "make babies",
+    aliases: [`makebb`, 'mb'],
+    execute(message, args){
+      const Discord = require('discord.js');
+      const prefix = ">";
+      const member = message.member;
+    const userToMakeBabyWith = message.mentions.members.first() || message.guild.members.get(args[1]);
+  message.channel.send(`${message.author.username} made a baby with ${userToLove}`);
+
+  }
+}
