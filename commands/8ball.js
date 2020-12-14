cff0e5db226b0b0075bae3af6bed8543e2e9caee
hayden <07htravers@mybest.org.uk>
2020-12-14 21:27:55 +0000

    Create 8ball.js

---
+++ commands/8ball.js
@@ -0,0 +1,39 @@
+const answers = [
+	'Maybe.',
+	'Certainly not.',
+	'I hope so.',
+	'Not in your wildest dreams.',
+	'There is a good chance.',
+	'Quite likely.',
+	'I think so.',
+	'I hope not.',
+	'I hope so.',
+	'Never!',
+	'Fuhgeddaboudit.',
+	'Ahaha! Really?!?',
+	'Pfft.',
+	'Sorry, bucko.',
+	'Hell, yes.',
+	'Hell to the no.',
+	'The future is bleak.',
+	'The future is uncertain.',
+	'I would rather not say.',
+	'Who cares?',
+	'Possibly.',
+	'Never, ever, ever.',
+	'There is a small chance.',
+	'Yes!'
+];
+module.exports = {
+    name: '8ball',
+    description: "this is an 8ball command",
+    execute(message, args){
+      const Discord = require('discord.js');
+      const prefix = ">";
+      
+      return msg.reply(question.join(' ').endsWith('?') ?
+      `🎱 ${answers[Math.floor(Math.random() * answers.length)]}` :
+      '🎱 That doesn\'t seem to be a question, please try again!');
+  
+  }
+}
