72159505a19cc0a33e6486ca54b9ff7ae1c74452
hayden <07htravers@mybest.org.uk>
2020-12-15 11:42:54 +0000

    Update sus.js

--- commands/sus.js
+++ commands/sus.js
@@ -1,10 +1,11 @@
 module.exports = {
     name: 'sus',
     description: "sus someone",
+    aliases: ['sus'],
     execute(message, args){
       const Discord = require('discord.js');
       const prefix = ">";
-      const member = message.member;
+      
     const sussedmemeber = message.mentions.members.first() || message.guild.members.get(args[1]);
     message.channel.send(` ${sussedmemeber} looking kinda sus tho`);
 
