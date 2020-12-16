fa1843aabbdbd1abe6bd4a5936684147dce8b790
hayden <07htravers@mybest.org.uk>
2020-12-16 11:26:20 +0000

    Update bonk.js

--- commands/bonk.js
+++ commands/bonk.js
@@ -10,8 +10,8 @@ module.exports = {
     const BonkedUser = message.mentions.members.first() || message.guild.members.get(args[1]);
     const BonkEmbed = new Discord.MessageEmbed()
     .setColor(message.member.displayColor)
-    .setTitle(`${message.author.username} Bonked ${BonkedUser}!`)
-    .setImage("https://tenor.com/view/yoshi-hammer-idiot-bonk-gif-17119858");
+    .setTitle(`${message.author.username} Bonked ${BonkedUser.user.username}!`)
+    .setImage("https://media.tenor.com/images/fcece71708fa38d267c857f44562a82d/tenor.gif");
     message.channel.send(BonkEmbed);
 
   }
