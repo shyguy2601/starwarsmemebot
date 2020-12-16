e4bbf41950c907673dce87dff93b93a73f151b3b
hayden <07htravers@mybest.org.uk>
2020-12-16 10:18:17 +0000

    Update index.js

--- index.js
+++ index.js
@@ -15,12 +15,17 @@ for(const file of commandFiles){
     Client.commands.set(command.name, command);
 
 }
+const DaysToGoArgs = message.content.slice(">".length).trim().split(/ +/).shift();
+
+Client.user.setPresence({ activity: { name: args.join(" "), type: 0 } });
 const activities_list = [ 
   //{ text: "Prefix is > , do >help", type: 0},
   //{ text: "I'm basically just a meme bot right now", type: 0},
+  { name: args.join(" "), type: 0 }, 
   { text: ">help", type: 0},
   { text: "Developed by ShyGuy#5504", type: 0},
-  { text: "you sleep 0_o", type: 3}
+  { text: "you sleep 0_o", type: 3},
+  { text: `${DaysToGo} days until Christmas!`, type: 0}
 ]
 
   let i = 0;
