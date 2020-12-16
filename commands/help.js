22f31ba9f2eb6d6a61ceae209a04b8d7e63fa875
hayden <07htravers@mybest.org.uk>
2020-12-16 15:36:55 +0000

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,56 +1,41 @@
-const Discord = require('discord.js');
-const { MessageEmbed } = require('discord.js')
-let help = require('../data/help.json');
+
+
 module.exports = {
   name: 'help',
   description: "this is a help command",
   execute(message, args) {
-      
+      const Discord = require('discord.js');
       const prefix = ">";
-      
-   
+      const { MessageEmbed } = require('discord.js');
+      const NextToCommand = "[] next to the command name is a mandatory thing for the command to work"
+      const insultExample = ">insult [ping the user you want to insult]"
 
-      const embed = new Discord.MessageEmbed()
-          .addField('`Fun Commands`', '😂', true)
-          .addField('`Information Commands`', '💻', true)
+      const helpEmbed = new Discord.MessageEmbed()
+          .setTitle(`Help - \`\`${NextToCommand}\`\``)
+          .addFields(
+          { name: '__Available Commands:-__', value: '\u200B' }, 
+          { name: '__Nitro:-__', value: '\u200B' }, 
+          { name: `>claim`, value: `Claim your free $9.99 discord nitro!`, inline: true},
+          { name: '__Information:-__', value: '\u200B'},
+          { name: '>userinfo / >whois / >ui', value: 'Shows the information of a member', inline: true},
+          //{ name: '>serverinfo / >si', value: 'Shows the information of the server', inline: true},
+          //{ name: '>botinfo / >bi / >info', value: 'Shows the information of the bot', inline: true},
+          { name: `__Meme Related Commands__`, value: '\u200B' }, 
+          { name: '>meme / >m', value: 'Get a random meme from reddit', inline: true }, 
+          { name: '>rickandmortymeme / >ramm', value: 'Get a random rick and morty meme from reddit', inline: true }, 
+          { name: '>minecraftmeme / >mcm', value: 'Get a random minecraft meme from reddit', inline: true }, 
+          { name: '>starwarsmeme / >swmeme / >swm', value: 'Get a starwars meme from reddit', inline: true }, 
+          { name: '>harrypottermeme / >hpmeme / >hpm', value: 'Get a harry potter meme from reddit', inline: true })
+        
+          
           .setTimestamp()
           .setFooter(`Requested by ${message.author.username}`);
-message.channel.send(embed).then((message) => {
-  message.react('🏠').then(() => message.react('😂')).then(() => message.react('💻')).then(()=> {
-    const filter = (reaction, user) => {
-      return ['🏠', '😂', '💻'].includes(reaction.emoji.name) && user.id === message.author.id;
-    };
-    const collector = message.createReactionCollector(filter, { time: 30000 });
-    collector.on('collect', (reaction, user) => {
-      console.log('Hello')
-      const embed = new Discord.MessageEmbed()
-      .addField('`Fun Commands`', '😂', true)
-      .addField('`Information Commands`', '💻', true)
-      .setTimestamp()
-      .setFooter(`Requested by ${message.author.username}`);
 
-      if(reaction.emoji.name === '🏠'){
-      embed.addField('`Fun Commands`', '😂', true)
-      embed.addField('`Information Commands`', '💻', true)
-      }else if(reaction.emoji.name === '😂'){
-        help["fun"].forEach(command =>{
-          embed.addField(command.name, command.description)
-        });
-      }else if(reaction.emoji.name === '💻'){
-        help["information"].forEach(command =>{
-          embed.addField(command.name, command.description)
-        });
-        message.edit(embed);
-        try { reaction.users.remove(message.author.id); } catch {}
-      }
-    })
-    collector.on('end', collected => {
-      try{
-        message.delete();
-        message.react('👌');
-      } catch {}
-    })
-  })
-})
+          if (message.guild.id == '396021514740301825')
+          helpEmbed.addFields({ name: '>entermeme', value: 'Enter a meme into meme-entrys to try win the meme competition', inline: true })
+
+      helpEmbed.addFields({ name: '__Fun Related Commands__', value: '\u200B' }, { name: `${insultExample}`, value: `Insults the mentioned user`, inline: true });
+
+      message.channel.send(helpEmbed);
   }
 } 
