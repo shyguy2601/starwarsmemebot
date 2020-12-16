734cffe74a5305655dc5e790e41a34a06d986dc5
hayden <07htravers@mybest.org.uk>
2020-12-16 12:26:57 +0000

    Update index.js

--- index.js
+++ index.js
@@ -17,21 +17,18 @@ for(const file of commandFiles){
 
 
 const activities_list = [ 
-  //{ text: "Prefix is > , do >help", type: 0},
-  //{ text: "I'm basically just a meme bot right now", type: 0},
   { text: ">help", type: 0},
   { text: "Developed by ShyGuy#5504", type: 0},
   { text: "9 Days until Christmas!", type: 0},
   { text: "you sleep 0_o", type: 3},
-  { text: "SAVE ME", type: 0}
-  ]
+  { text: "SAVE ME", type: 0}]
 
   let i = 0;
 
 Client.once('ready', () => {
   console.log('Online!');
   setInterval(() => {
-      if (i > activities_list.length) { i = 0 }
+    if( i === activities.length )
       Client.user.setPresence({ activity: { name: activities_list[i].text, type: activities_list[i].type } });
       i++; 
   }, 10000);
