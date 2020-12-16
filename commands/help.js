97c7748f8cea92874ef3cf13472decee0358abe2
hayden <07htravers@mybest.org.uk>
2020-12-16 19:18:22 +0000

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -11,14 +11,15 @@ module.exports = {
         const embed = new Discord.MessageEmbed()
             .addField('`Fun Commands `', '😂', true)
             .addField('`Information Commands `', '💻', true)
+            .addField('`Utilities`', '🧰', true)
             .setTimestamp()
             .setFooter(`Requested by ${message.author.username}`)
             //.setAuthor("Original help command made by macedonga#5797", "https://cdn.macedon.ga/p.n.g.r.png");
 
         message.channel.send(embed).then((msg) => {
-            msg.react('🏠').then(() => msg.react('😂')).then(() => msg.react('💻')).then(() => {
+            msg.react('🏠').then(() => msg.react('😂')).then(() => msg.react('💻')).then(() => msg.react('🧰)')).then(() => {
                 const filter = (reaction, user) => {
-                    return ['🏠', '😂', '💻'].includes(reaction.emoji.name) && user.id === message.author.id;
+                    return ['🏠', '😂', '💻', '🧰)'].includes(reaction.emoji.name) && user.id === message.author.id;
                 };
                 const collector = msg.createReactionCollector(filter, { time: 30000 });
                 collector.on('collect', (reaction, user) => {
@@ -31,6 +32,7 @@ module.exports = {
                     if (reaction.emoji.name === '🏠') {
                         embed.addField('`Fun Commands `', '😂', true)
                         embed.addField('`Information Commands `', '💻', true)
+                        embed.addField('`Utilities`', '🧰)', true)
                     } else if (reaction.emoji.name === '😂') {
                         help["fun"].forEach(command => {
                             embed.addField(command.name, command.description)
@@ -39,16 +41,16 @@ module.exports = {
                         help["information"].forEach(command => {
                             embed.addField(command.name, command.description)
                         });
+                    } else if(reaction.emoji.name === '🧰)'){
+                      help["utilities"].forEach(command =>{
+                        embed.addField(command.name, command.description)
+                      }
+                        )
                     }
                     msg.edit(embed);
                     try { reaction.users.remove(message.author.id); } catch {}
                 });
-                /*collector.on('end', collected => {
-                    try {
-                        msg.delete();
-                        message.react('👌');
-                    } catch {}
-                })*/
+              
             })
         })
     }
