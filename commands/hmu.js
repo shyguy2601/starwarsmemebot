61da920f8586d61fcc8280b4695274a7c8cd8f6a
hayden <07htravers@mybest.org.uk>
2020-12-21 12:57:36 +0000

    Update hmu.js

--- commands/hmu.js
+++ commands/hmu.js
@@ -10,7 +10,9 @@ module.exports = {
     .setColor(message.member.displayColor)
     .setTitle(`${message.author.username} is sad and lonely and needs some love`)
     .setDescription(`Pls give ${message.author.username} their daily dose of love and attention`)
-
+    message.delete()
+    .then(msg => console.log(`Deleted message from ${msg.author.username}`))
+    .catch(console.error);
   message.channel.send(ImLonely);
   }
 }
