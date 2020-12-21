20d961c2f616861d57e862b3ca0fcdf72873289f
hayden <07htravers@mybest.org.uk>
2020-12-21 13:53:56 +0000

    Create avatar.js

---
+++ commands/avatar.js
@@ -0,0 +1,29 @@
+module.exports = {
+  name: 'avatar',
+  description: "Get's a user's avatar",
+  execute(message, args){
+    const getUserFromMention = mention => {
+      if (!mention || !mention.startsWith('<@')|| !mention.endsWith('>')) return;
+      mention = mention.slice(2, -1);
+      if (mention.startsWith('!')) mention = mention.slice(1);
+      return client.users.cache.get(mention);
+   }
+  
+      
+    if (args[0]) {
+      
+      const user = getUserFromMention(args[0]);
+
+      if (!user) {
+          return message.reply('Please use a proper mention if you want to see someone else\'s avatar.');
+      }
+
+      return message.channel.send(`${user.username}'s avatar: ${user.displayAvatarURL({ dynamic: true })}`);
+  }
+
+  return message.channel.send(`${message.author.username}, your avatar: ${message.author.displayAvatarURL({ dynamic: true })}`);
+
+
+}
+
+}
