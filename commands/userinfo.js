0073238835384c499637b7a31027ff10024adcd6
hayden <07htravers@mybest.org.uk>
2020-12-21 16:15:36 +0000

    Update userinfo.js

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -26,13 +26,10 @@ module.exports = {
         let member = message.mentions.members.first() || message.guild.members.cache.find(user => user.displayName == args[1]) || message.guild.members.cache.find(user => user.id == args[1]);
      
         if (!member) {
-            if (!options[1]) {
+            
                 return message.reply("Please mention a user!.")
-            }
-            let userId = parseInt(options[1]);
-            if (Number.isInteger(userId) == true) {
-                member = message.guild.members.fetch(options[1]);
-            }
+            
+           
         }
     const roles = member.roles.cache    
         .sort((a, b) => b.position - a.position)
