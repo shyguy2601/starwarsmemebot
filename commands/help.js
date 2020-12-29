47cea3051b3722f1c9e9c4515b146c876c8bb6e7
hayden <07htravers@mybest.org.uk>
2020-12-29 19:19:26 +0000

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -10,7 +10,8 @@ module.exports = {
 
         const embed = new Discord.MessageEmbed()
             .setThumbnail(member.user.displayAvatarURL({ dynamic: true, size: 512}))
-            .addField(' ',`Join the support server if you are confused:-(https://discord.gg/hcpK8kvjPc) `)
+            .addField(
+                {name: 'Join the support server if you are confused', value:`(https://discord.gg/hcpK8kvjPc)`})
             .setColor(message.member.displayColor)
             .setTitle('Help')
             .addField('`Fun Commands `', '😂', true)
