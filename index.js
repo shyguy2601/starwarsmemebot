5073a5e06379115605d571ca9a9959d6d9727266
hayden <07htravers@mybest.org.uk>
2020-12-29 12:57:49 +0000

    Update index.js

--- index.js
+++ index.js
@@ -18,9 +18,8 @@ for(const file of commandFiles){
 const activities_list = [ 
   { text: ">help", type: 0},
   { text: "Developed by ShyGuy#5504", type: 0},
-  { text: "Merry Christmas!", type: 0},
   { text: "you sleep 0_o", type: 3},
-  { text: "31 days until Shyguy's 18th birthday!!", type: 0}]
+  { text: "28 days until Shyguy's 18th birthday!!", type: 0}]
 
   let i = 0;
 
