d732fe24d14ba9ec535b96fac75b462a5970a049
hayden <07htravers@mybest.org.uk>
2020-12-29 21:34:52 +0000

    Update servercount.js

--- commands/servercount.js
+++ commands/servercount.js
@@ -6,18 +6,15 @@ module.exports ={
     aliases: ['sc'],
     execute(message,args,client){
         const guilds = client.guilds.cache.array()
-       const UptimeEmbed = new Discord.MessageEmbed()
+       const ServerCountEmbed = new Discord.MessageEmbed()
  
                  .setColor(`#FFD0F0`)
                  .setTitle(`Uptime`)
  
-       let days = Math.floor(client.uptime / 86400000);
-       let hours = Math.floor(client.uptime / 3600000) % 24;
-       let minutes = Math.floor(client.uptime / 60000) % 60;
-       let seconds = Math.floor(client.uptime / 1000) % 60;    
+       
  
-       UptimeEmbed.setDescription(`\`\`\`\nServer Count : ${guilds.length}\`\`\``);
+       ServerCountEmbed.setDescription(`\`\`\`\nServer Count : ${guilds.length}\`\`\``);
  
-       message.channel.send(UptimeEmbed)
+       message.channel.send(ServerCountEmbed)
     }
 } 
