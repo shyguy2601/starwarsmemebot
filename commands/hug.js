24cf3fd5ec9c9754be397341bf4a6476d73179b4
hayden <07htravers@mybest.org.uk>
2020-12-30 10:40:38 +0000

    Update hug.js

--- commands/hug.js
+++ commands/hug.js
@@ -5,9 +5,14 @@ module.exports = {
     execute(message, args){
       const Discord = require('discord.js');
       const prefix = ">";
-      const member = message.member;
+      if(message.channel.type == 'text'){
+        const member = message.member;
     const userToHug = message.mentions.members.first() || message.guild.members.get(args[1]);
   message.channel.send(`${message.author.username} hugged ${userToHug}`);
+      }if(message.channel.type == 'dm'){
+        return message.channel.send(`Sorry this command doesn't work in DMs`);
+      }
+      
 
   }
 }
