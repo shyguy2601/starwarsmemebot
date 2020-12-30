1a1791610d53c3a98b73e376a06f217a9a3b3507
hayden <07htravers@mybest.org.uk>
2020-12-30 10:05:24 +0000

    Update servercount.js
    Made servercount command DM friendly

--- commands/servercount.js
+++ commands/servercount.js
@@ -5,16 +5,21 @@ module.exports ={
     description: "A server count command",
     aliases: ['sc'],
     execute(message,args,client){
+        if(message.channel.type == "text"){
         const guilds = client.guilds.cache.array()
        const ServerCountEmbed = new Discord.MessageEmbed()
- 
-                 .setColor(`#FFD0F0`)
-                 .setTitle(`Server Count`)
- 
-       
- 
-       ServerCountEmbed.setDescription(`\`\`\`\nServer Count : ${guilds.length}\`\`\``);
+        .setColor(`#FFD0F0`)
+        .setTitle(`Server Count`)
+        ServerCountEmbed.setDescription(`\`\`\`\nServer Count : ${guilds.length}\`\`\``);
  
        message.channel.send(ServerCountEmbed)
+        }if(message.channel.type == "dm"){
+            const guilds = client.guilds.cache.array()
+            const ServerCountEmbed = new Discord.MessageEmbed()
+             .setColor(`#FFD0F0`)
+             .setTitle(`Server Count`)
+             ServerCountEmbed.setDescription(`\`\`\`\nServer Count : ${guilds.length}\`\`\``);
+
+        }
     }
 } 
