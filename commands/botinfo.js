d93eeff2d428f788fafa7e9ec38cb69f824135f2
hayden <07htravers@mybest.org.uk>
2020-12-30 09:57:10 +0000

    Update botinfo.js
    Made botinfo command DM friendly

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -10,16 +10,17 @@ module.exports = {
     description: "gets information about the bot",
     aliases: ['bi', 'botinfo'],
     execute: (message, args, client) => {
+        if(message.channel.type == "text"){
     const guilds = client.guilds.cache.array()
     const core = os.cpus()[0];
 
-        let days = Math.floor(client.uptime / 86400000);
-        let hours = Math.floor(client.uptime / 3600000) % 24;
-        let minutes = Math.floor(client.uptime / 60000) % 60;
-        let seconds = Math.floor(client.uptime / 1000) % 60;    
-    const embed = new Discord.MessageEmbed()
-        .setColor(message.guild.me.displayHexColor || 'BLUE')
-        .addField('General', [
+            let days = Math.floor(client.uptime / 86400000);
+            let hours = Math.floor(client.uptime / 3600000) % 24;
+            let minutes = Math.floor(client.uptime / 60000) % 60;
+            let seconds = Math.floor(client.uptime / 1000) % 60;    
+            const embed = new Discord.MessageEmbed()
+            .setColor(message.guild.me.displayHexColor || 'BLUE')
+            .addField('General', [
             `**> Commands:** ${client.commands.size}`,
             `**> Server Count:** ${client.guilds.cache.size}`,
             `**> Users:** ${client.guilds.cache.reduce((a, b) => a + b.memberCount, 0)}`,
@@ -38,5 +39,34 @@ module.exports = {
         .setTimestamp()
         
         message.channel.send(embed);
+    }if(message.channel.type == "dm"){
+        const embed = new Discord.MessageEmbed()
+        .setColor('#DC143C')
+        .addField('General', [
+        `**> Commands:** ${client.commands.size}`,
+        `**> Server Count:** ${client.guilds.cache.size}`,
+        `**> Users:** ${client.guilds.cache.reduce((a, b) => a + b.memberCount, 0)}`,
+        `**> Channels:** ${client.channels.cache.size}`,
+        `**> Creation Date:** ${utc(client.user.createdTimestamp).format('Do MMMM YYYYY HH:mm:ss')}`,
+        `**> Node.js Version:** ${process.version}`,
+        `**> Discord.js Version:** v${djsversion}`,
+        `**> Bot Creator:** ShyGuy#5504`,
+        '\u200B'
+    ])
+    .addField('System', [
+        `**> Platform:** ${process.platform}`,
+        `**> Uptime:** ${days} days, ${hours} hours, ${minutes} minutes, ${seconds} seconds`,
+        
+    ])
+    .setTimestamp()
+    
+    message.channel.send(embed);
+
+
+
+
+
+
+    }
     }
 };
