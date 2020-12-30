d1b61928b7b8d83aa53d841d525610eca2a0be21
hayden <07htravers@mybest.org.uk>
2020-12-30 10:45:34 +0000

    Update kiss.js

--- commands/kiss.js
+++ commands/kiss.js
@@ -5,9 +5,14 @@ module.exports = {
     execute(message, args){
       const Discord = require('discord.js');
       const prefix = ">";
-      const member = message.member;
+      if(message.channel.type == 'text'){
+        const member = message.member;
     const userToLove = message.mentions.members.first() || message.guild.members.get(args[1]);
   message.channel.send(`${message.author.username} kissed ${userToLove}`);
+      }if(message.channel.type == 'dm'){
+        return message.channel.send('Sorry this command does not work in DMs');
+      }
+      
 
   }
 }
