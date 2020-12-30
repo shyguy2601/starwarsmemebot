d4acab83ec50b4b45ebb7f51c24fcddc84dbf163
hayden <07htravers@mybest.org.uk>
2020-12-30 14:16:11 +0000

    Update message.js

--- commands/message.js
+++ commands/message.js
@@ -12,30 +12,37 @@ module.exports = {
   .setDescription(`The message was successfully sent to ShyGuy`)
   .setAuthor(`Server:- ${message.guild.name}`);
   
+
   const couldnotmessageuserembed = new Discord.MessageEmbed()
   .setColor(0x00F5FF)
   .setTitle('Error Occurred')
   .setDescription('I was unable to message shyguy, their DMs are off')
-  const usertomessage = message.mentions.members.first() || message.guild.members.get(args[1]);
-  if(args[1].includes('704708159901663302'))
-    return message.channel.send('You can only message the bot owner ShyGuy')
-  
+
+  const member = message.mentions.members.first() || message.guild.members.cache.find(user => user.displayName == args[1]) || message.guild.members.cache.find(user => user.id == '704708159901663302');
+  if(args[1] != '704708159901663302'){
+    message.channel.send('You can only message the bot owner ShyGuy')
+    return;
+  }
+  if(member.user.id != '704708159901663302') {
+      message.channel.send('You can only message the bot owner ShyGuy')
+      return;
+  }
 let messagetosend = args.slice(2).join(" ");
     if(!messagetosend) return  message.channel.send("No message given to send to the user");
 
     const usermessageembed = new Discord.MessageEmbed()
       .setColor(0x00F5FF)
       .setDescription(`${message.author.username} from the server ***${message.guild.name}*** said ${messagetosend}`);
+      member.send(usermessageembed)
+      .then(embed => {
+        embed.react('✉️')
+     
       
-      message.client.users.fetch("704708159901663302").then(user=>{
-        user.send(usermessageembed)
-        .then(embed =>{
-          embed.react('✉️')
-          message.channel.send(userwasmessagedembed);
-        })
-        .catch(() => message.channel.send(couldnotmessageuserembed));
-      })
-      .catch(() => message.channel.send("Error in fetch"));
+      
+      message.channel.send(userwasmessagedembed);
+    }) 
+    .catch(() => message.channel.send(couldnotmessageuserembed))
+      
   }
 }    
     
