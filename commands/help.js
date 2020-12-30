b66631d1e5a41c5623f121e55c7dab457b510861
hayden <07htravers@mybest.org.uk>
2020-12-30 10:27:43 +0000

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -71,69 +71,7 @@ module.exports = {
             })
         }) 
         }if(message.channel.type == "dm"){
-
-            const member = message.author;
-        
-
-            const embed = new Discord.MessageEmbed()
-                .addField('\u200B',`Join the support server if you are confused:- (https://discord.gg/hcpK8kvjPc)`)
-                .setColor('#008000')
-                .setTitle('Help')
-                .addField('`Fun Commands `', '😂', true)
-                .addField('`More Fun Commands `', '😭', true)
-                .addField('`Information Commands `', '💻', true)
-                .addField('`Utilities`', '🔨', true)
-                .setTimestamp()
-                .setFooter(`Requested by ${message.author.username}`)
-                //.setAuthor("Original help command made by macedonga#5797", "https://cdn.macedon.ga/p.n.g.r.png");
-    
-            message.channel.send(embed).then((msg) => {
-                msg.react('🏠').then(() => msg.react('😂')).then(() => msg.react('😭')).then(() => msg.react('💻')).then(() => msg.react('🔨')).then(() => {
-                    const filter = (reaction, user) => {
-                        return ['🏠', '😂','😭', '💻', '🔨'].includes(reaction.emoji.name) && user.id === message.author.id;
-                    };
-                    const collector = msg.createReactionCollector(filter, { time: 30000 });
-                    collector.on('collect', (reaction, user) => {
-                        
-                        const embed = new Discord.MessageEmbed()
-                            .setColor('#008000')
-                            .setTimestamp()
-                            .setFooter(`Requested by ${message.author.username} `)
-                            //.setAuthor("Original help command made by macedonga#5797", "https://cdn.macedon.ga/p.n.g.r.png");
-    
-                        if (reaction.emoji.name === '🏠') {
-                            embed.addField('\u200B',`Join the support server if you are confused:- (https://discord.gg/hcpK8kvjPc)`)
-                            .setColor('#008000')
-                            embed.setTitle('Help')
-                            embed.addField('`Fun Commands `', '😂', true)
-                            .addField('`More Fun Commands `', '😭', true)
-                            embed.addField('`Information Commands `', '💻', true)
-                            embed.addField('`Utilities`', '🔨', true)
-                        } else if (reaction.emoji.name === '😂') {
-                            help["fun"].forEach(command => {
-                                embed.addField(command.name, command.description)
-                            });
-                        }else if(reaction.emoji.name === '😭') {
-                         help["extra"].forEach(command =>{
-                             embed.addField(command.name, command.description)
-                         })   
-                        }else if (reaction.emoji.name === '💻') {
-                            help["information"].forEach(command => {
-                                embed.addField(command.name, command.description)
-                            });
-                        } else if(reaction.emoji.name === '🔨'){
-                          help["utilities"].forEach(command =>{
-                            embed.addField(command.name, command.description)
-                          }
-                            )
-                        }
-                        msg.edit(embed);
-                        try { reaction.users.remove(message.author.id); } catch {}
-                    });
-                  
-                })
-            }) 
-            
+          message.channel.send(`Sorry this command doesn't work in DMs`)
         }
 
     }
