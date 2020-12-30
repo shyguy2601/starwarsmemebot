67d8e43d6df7e2c23ed92361cfda673f54bbc1a0
hayden <07htravers@mybest.org.uk>
2020-12-30 14:39:31 +0000

    Created botusers.js

---
+++ commands/botusers.js
@@ -0,0 +1,26 @@
+const { MessageEmbed } = require('discord.js');
+const Discord = require('discord.js');
+module.exports ={
+    name: 'botusers',
+    description: "Says the amount of users the bot has",
+    aliases: ['bu'],
+    execute(message,args,client){
+        if(message.channel.type == "text"){
+        const guilds = client.guilds.cache.array()
+       const BotUsersEmbed = new Discord.MessageEmbed()
+        .setColor(`#FFD0F0`)
+        .setTitle(`Bot Users`)
+        BotUsersEmbed.setDescription(`\`\`\`\nBot Users : ${client.guilds.cache.reduce((a, b) => a + b.memberCount, 0)}\`\`\``);
+ 
+       message.channel.send(BotUsersEmbed)
+        }if(message.channel.type == "dm"){
+            const guilds = client.guilds.cache.array()
+            const BotUsersEmbed = new Discord.MessageEmbed()
+            .setColor(`#FFD0F0`)
+            .setTitle(`Bot Users`)
+            BotUsersEmbed.setDescription(`\`\`\`\nBot Users : ${client.guilds.cache.reduce((a, b) => a + b.memberCount, 0)}\`\`\``);
+     
+           message.channel.send(BotUsersEmbed)
+        }
+    }
+} 
