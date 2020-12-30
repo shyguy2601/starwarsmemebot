1d89f1b14e7214315072d01e866671caa16fc74d
hayden <07htravers@mybest.org.uk>
2020-12-30 10:01:53 +0000

    Update botinfo.js

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -40,6 +40,10 @@ module.exports = {
         
         message.channel.send(embed);
     }if(message.channel.type == "dm"){
+            let days = Math.floor(client.uptime / 86400000);
+            let hours = Math.floor(client.uptime / 3600000) % 24;
+            let minutes = Math.floor(client.uptime / 60000) % 60;
+            let seconds = Math.floor(client.uptime / 1000) % 60;    
         const embed = new Discord.MessageEmbed()
         .setColor('#DC143C')
         .addField('General', [
