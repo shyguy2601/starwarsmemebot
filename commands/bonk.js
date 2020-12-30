427bf7500ad0629945ed11587b9b32e705b23ce4
hayden <07htravers@mybest.org.uk>
2020-12-30 10:09:59 +0000

    Update bonk.js
    Made bonk command non executable from DMs

--- commands/bonk.js
+++ commands/bonk.js
@@ -6,7 +6,7 @@ module.exports = {
     execute(message, args){
       const Discord = require('discord.js');
       const prefix = ">";
-      
+      if(message.channel.type == "text"){
     const BonkedUser = message.mentions.members.first() || message.guild.members.get(args[1]);
     if(BonkedUser.id == "704708159901663302") {
     message.channel.send("Sorry but i cant bonk my owner") 
@@ -17,6 +17,9 @@ module.exports = {
     .setTitle(`${message.author.username} Bonked ${BonkedUser.user.username}!`)
     .setImage("https://media.tenor.com/images/fcece71708fa38d267c857f44562a82d/tenor.gif");
     message.channel.send(BonkEmbed);
+      }if(message.channel.type == "dm"){
 
+        message.channel.send(`Sorry this command doesn't work in DMs`)
+      }
   }
 }
