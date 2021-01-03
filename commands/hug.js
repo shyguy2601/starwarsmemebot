59f0075025a234c5aa2906360883142533f0bf8c
hayden <07htravers@mybest.org.uk>
2021-01-03 13:04:14 +0000

    Update hug.js

--- commands/hug.js
+++ commands/hug.js
@@ -7,6 +7,12 @@ module.exports = {
     name: 'hug',
     description: "hug another user",
       execute: async(message, args) => {
+        const hug_gifs = [
+          'https://tenor.com/view/milk-and-mocha-bear-couple-line-hug-cant-breathe-gif-12687187',
+          'https://tenor.com/view/milk-and-mocha-hug-cute-kawaii-love-gif-12535134',
+          'https://tenor.com/view/peach-cat-hug-hug-up-love-mochi-mochi-peach-cat-gif-16334628',
+          'https://tenor.com/view/hug-love-hi-bye-cat-gif-15999080'
+        ]
      if (!message.author.bot) {
 let hugged = message.mentions.users.first();
 if(!hugged) {
@@ -17,13 +23,13 @@ return;
 } else {
   
       const author = message.author.username;
-      fetch('https://nekos.life/api/v2/img/hug').then((data)=>{
-            if (!(data || data.url)) return message.sendError('NO_DATA');
+      
           const Embed = new MessageEmbed()
-          .setImage(data.url)
           .setTitle(`${message.author.username} hugged ${hugged.username}`) //hug msg
+          .setImage(`${hug_gifs[Math.floor(Math.random() * hug_gifs.length)]}`)
+          
           message.channel.send(Embed)
-    });
+    
  
    
        }
