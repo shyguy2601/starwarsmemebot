1797b15b1161b737ddff433221150d21857009e3
hayden <07htravers@mybest.org.uk>
2021-01-03 12:06:50 +0000

    Code Refactoring

--- index.js
+++ index.js
@@ -69,33 +69,6 @@ Client.once('ready', () => {
       require(`./slash_commands/${interaction.data.name}.js`)(client, interaction);
   })
 
-    Client.on('message', message => {
-      const args = message.content.trim().split(/ +/g);
-      
-        const commandName = args[0].slice(prefix.length).toLowerCase();
-      
-      
-        const command = Client.commands.get(commandName) || Client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
-      /*if(message.content == 'what did i win' || message.content == 'What did i win'|| message.content == 'what did i win?' || message.content == 'What did i win?'){
-        message.reply(`You won nothing you greedy bastard, it's just for fun`)
-      }
-      if((args[0].toLowerCase() == 'i\'m' || args[0].toLowerCase() == 'im') && (args[1])){
-        if(args[1].toLowerCase() == 'dad'){
-          return message.channel.send(`Impossible, you can't be dad, because i'm dad`);
-        }
-        message.channel.send(`Hi ${args.slice(1).join(' ')}, I'm dad`);
-      }
-      if (message.content == `pls meme` || message.content==`Pls meme`){
-        message.reply('***Pls meme is disabled use >meme instead***')
-    }*/
     
-        if(!message.content.startsWith(`${prefix}`)) return;
-        if (message.author.bot) return;
-        if(!command) return;
-      
-        command.execute(message, args, Client);
-        
-        
-      });
     
 Client.login(process.env.DJS_TOKEN);
