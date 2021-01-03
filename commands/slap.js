e2cbfa30eeee2f4246e346a122f02cee04a66eb2
hayden <07htravers@mybest.org.uk>
2021-01-03 13:51:03 +0000

    Update slap.js

--- commands/slap.js
+++ commands/slap.js
@@ -1,17 +1,33 @@
+const fetch = require('node-fetch');
+const { MessageEmbed } = require('discord.js');
+
 module.exports = {
     name: 'slap',
-    description: "slap someone",
-    aliases: [`s`,],
-    execute(message, args){
-      const Discord = require('discord.js');
-      const prefix = ">";
-      const member = message.member;
-    const userToSlap = message.mentions.members.first() || message.guild.members.get(args[1]);
-    if(userToSlap.id == "704708159901663302") {
-    message.channel.send("Sorry but i cant slap my owner") 
-    return;
-} else if(userToSlap.id != "704708159901663302") {
-    message.channel.send(`${message.author.username} Slapped ${userToSlap} hard across the face and ${userToSlap} cried`);
+    description: "slap another user",
+    execute: (message, args) => {
+      if(message.channel.type == 'dm'){
+        return message.channel.send('Sorry this command does not work in DMs');
+      }
+      if(userToSlap.id == "704708159901663302") {            //ignore this for someone bot
+        message.channel.send("Sorry but i cant slap my owner") 
+        return;
     }
-  }
+        if (!message.author.bot) {
+            let slapped = message.mentions.users.first();
+            if (!slapped)
+                return message.channel.send(`Mention a user to slap!`)
+            else {
+                const author = message.author.username;
+                fetch('https://nekos.life/api/v2/img/slap')
+                    .then(res => res.json())
+                    .then(json => {
+                        const Embed = new MessageEmbed()
+                            .setImage(json.url)
+                            .setTitle(`${message.author.username} kissed ${slapped.username}`) //hug msg
+                        message.channel.send(Embed)
+                    })
+                    .catch(err => console.log(err));
+            }
+        }
+    }
 }
