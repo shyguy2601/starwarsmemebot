eba4b8ffac96f60a8d57d2e18217358ea64ee331
hayden <07htravers@mybest.org.uk>
2021-01-04 10:58:00 +0000

    Update sus.js

--- commands/sus.js
+++ commands/sus.js
@@ -1,12 +1,11 @@
 module.exports = {
     name: 'sus',
     description: "sus someone",
-    aliases: ['sus'],
-    execute(message, args){
+      execute(message, args){
       const Discord = require('discord.js');
-      const prefix = ">";
       
-    const sussedMember = message.mentions.members.first() || message.guild.members.get(args[1]);
+      
+      const sussedMember = message.mentions.members.first(); 
     if(!sussedMember){
       message.channel.send('You need to mention a user to call out for being sus!')
       return;
