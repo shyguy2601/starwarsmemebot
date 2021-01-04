0861d24333252d9780d2b5b605e115dded7462aa
hayden <07htravers@mybest.org.uk>
2021-01-04 10:52:24 +0000

    Update makebaby.js

--- commands/makebaby.js
+++ commands/makebaby.js
@@ -5,11 +5,15 @@ module.exports = {
     execute(message, args){
       const Discord = require('discord.js');
       const prefix = ">";
-      if(message.channel.type == 'text'){
+      
       const member = message.member;
     const userToMakeBabyWith = message.mentions.members.first(); 
+    if(!userToMakeBabyWith){
+      message.channel.send('You need to mention a user to make a baby with!')
+      return;
+    }
   message.channel.send(`${message.author.username} made a baby with ${userToMakeBabyWith}`);
-      }if(message.channel.type == 'dm'){
+      if(message.channel.type == 'dm'){
         return message.channel.send('Sorry this command does not work in DMs');
       }
   }
