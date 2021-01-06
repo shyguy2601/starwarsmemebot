9ad9484dbba576a8764f92820c0111a6e2432235
hayden <07htravers@mybest.org.uk>
2021-01-06 19:03:12 +0000

    Added play command

---
+++ commands/play.js
@@ -0,0 +1,37 @@
+const ytdl = require('ytdl-core')
+const ytSearch = require('yt-search')
+module.exports = {
+    name: 'play',
+    aliases: ['p'],
+    async execute(message, args) {
+       const voiceChannel = message.member.voice.channel;
+       if (!voiceChannel) return message.channel.send('You need to be in a voice channel')
+        const permissions = voiceChannel.permissionsFor(message.client.user);
+        if (!permissions.has('CONNECT')) return message.channel.send(`You don't have the correct permissions to use this`);
+        if (!permissions.has('SPEAK')) return message.channel.send(`You don't have the correct permissions to use this`);
+        if (!args.length) return message.channel.send('You need to send the second argument!');
+
+
+        const connection = await voiceChannel.join();
+
+        const videoFinder = async (query) => {
+            const videoResult = await ytSearch(query);
+
+            return (videoResult.videos.length > 1) ? videoResult.videos[0] : null;
+
+        }
+        const video = await videoFinder(args.join(' '));
+
+        if(video){
+            const stream = ytdl(video.url, {filter: 'audioonly'});
+            connection.play(stream, {seek: 0, volume: 1})
+            .on('finish', () =>{
+                voiceChannel.leave()
+            })
+            await message.reply(`:thumbsup: Now Playing ***${video.title}***`)
+        } else {
+            message.channel.send('No video results found')
+            return;
+        }
+    },
+};
--- commands/streamer.js
+++ commands/streamer.js
@@ -1,12 +1,17 @@
 module.exports = {
-    name: 'streamer',
-    description: "Get's Shine_ee's twitch",
+    name: 'ineedhelp',
+    description: "Gets Mental Health support links",
+    aliases: ['helpme'],
     
     execute(message, args){
       const Discord = require('discord.js');
-      const prefix = ">";
-      
-    message.channel.send(`https://www.twitch.tv/shine_ee`);
+      const { MessageEmbed } = require('discord.js');
+     
+      const mentalhealthhelpembed = new Discord.MessageEmbed()
+      .setTitle(`You don't have to struggle through this alone`)
+      .addField(`\u200b`,`https://www.depressioncenter.org/toolkit/im-looking-more-resources-1/helpful-mental-health-resources-0`)
+
+      message.channel.send(mentalhealthhelpembed)
 
   }
 }
