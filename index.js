a41945bc9dda2c63fdd47139822cf17839952a97
hayden <07htravers@mybest.org.uk>
2021-01-06 15:21:12 +0000

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const activities_list = [
   { text: ">help", type: 0},
   { text: "Developed by ShyGuy#5504", type: 0},
   { text: "you sleep 0_o", type: 3},
-  { text: "21 days until Shyguy's 18th birthday!!", type: 0}]
+  { text: "20 days until Shyguy's 18th birthday!!", type: 0}]
 
   let i = 0;
 
