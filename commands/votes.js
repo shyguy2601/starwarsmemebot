ad9ec73d1d621ee22c819a91bc8ac92593b6d43d
hayden <07htravers@mybest.org.uk>
2021-01-13 13:21:41 +0000

    Update votes.js

--- commands/votes.js
+++ commands/votes.js
@@ -1,4 +1,4 @@
-const Discord = require('discord.js');
+/*const Discord = require('discord.js');
 const { MessageEmbed } = require('discord.js');
 const Client = new Discord.Client();
 const Topgg = require('@top-gg/sdk')
@@ -30,3 +30,4 @@ module.exports = {
     message.channel.send(VoteEmbed);
   }
 }
+*/
