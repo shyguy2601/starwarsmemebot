7e8a827f765a3a05faa2fedbeb4635dc0d431a50
hayden <07htravers@mybest.org.uk>
2021-01-13 13:21:24 +0000

    Update vote.js

--- commands/vote.js
+++ commands/vote.js
@@ -1,17 +1,33 @@
 const Discord = require('discord.js');
 const { MessageEmbed } = require('discord.js');
 const Client = new Discord.Client();
+const Topgg = require('@top-gg/sdk')
+const api = new Topgg.Api('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Ijc4MzYyOTI3NTU0NjY0ODU3NyIsImJvdCI6dHJ1ZSwiaWF0IjoxNjEwNTM2NjQ2fQ.VjiMY2oLnuCVIkchA1el0tO383TuRGVTc6YBjINthg4')
 module.exports = {
     name: 'vote',
     description: "Get's the vote link",
-    execute(message, args){
-    const VoteEmbed = new Discord.MessageEmbed()
-    .setColor(message.member.displayColor)
-    .setTitle('Vote')
-    .setDescription('Please vote for me on top.gg')
-    .addField(`\u200B`,`<:vmkittyblush:798856313961840672>`)
-    .addField('Vote Link',`https://top.gg/bot/783629275546648577/vote`)
-    message.channel.send(VoteEmbed);
+   async execute(message, args){
+    
+    let has_voted;
+
+    let votes = [];
+
+    let apiVotes = await api.getVotes();
+    for (let i = 0; i < 10; i++) {
+      votes.push(i+1 + " - " + apiVotes[i]["username"] + "#" + apiVotes[i]["discriminator"]);
+    }
+
+    let canVote = await api.hasVoted(message.author.id)
+
+    if (canVote) { has_voted = `<a:check_yes:798896012773031938> You have voted for **[VibeMaster](https://top.gg/bot/783629275546648577/vote)**!`; } else { has_voted = `<a:check_no:798895971265675294> You have not voted for **[VibeMaster](https://top.gg/bot/783629275546648577/vote)**!` }
+    let vote_for_vibemaster = `**[Click Me To Vote](https://top.gg/bot/783629275546648577/vote)**`
+  const VoteEmbed = new Discord.MessageEmbed()
+    .setColor('#3586cd')
+    .setTitle('Vote for VibeMaster')
+    .addField(`Vote for me `, vote_for_vibemaster)
+    .setDescription("```css\n[ RECENT VOTES ]\n\n" + votes.join('\n') + "```")
+    .addField('Vote Status', has_voted);
+  message.channel.send(VoteEmbed);
 
   
   }
