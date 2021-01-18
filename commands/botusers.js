fb41fbd76e433a25f9a3b4d0d0bea43a2a062d80
hayden <07htravers@mybest.org.uk>
2021-01-18 15:34:34 +0000

    Update botusers.js

--- commands/botusers.js
+++ commands/botusers.js
@@ -6,7 +6,7 @@ module.exports ={
     description: "Says the amount of users the bot has",
     aliases: ['bu'],
     async execute(message,args, client){
-        let guilds = client.guilds.cache
+        let guilds = await client.guilds.cache
         guilds = guilds.array()
         if(message.channel.type == "text"){
      
