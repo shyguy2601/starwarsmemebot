bbc886f45f3a65c85e6730b9426b5658bcad9ec6
hayden <07htravers@mybest.org.uk>
2021-01-18 15:29:47 +0000

    Update botusers.js

--- commands/botusers.js
+++ commands/botusers.js
@@ -14,6 +14,7 @@ module.exports ={
         .setColor(`#FFD0F0`)
         .setTitle(`Bot Users`)
         BotUsersEmbed.setDescription(`\`\`\`\nBot Users : ${client.guilds.cache.reduce((a, b) => a + b.memberCount, 0)}\`\`\``);
+        console.log(Client.guilds.cache.reduce((a, b) => a + b.memberCount, 0))
  
        message.channel.send(BotUsersEmbed)
         }if(message.channel.type == "dm"){
