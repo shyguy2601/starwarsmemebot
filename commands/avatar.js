3aa1483e7229b7210ba707a1d1f748ca8498510c
hayden <07htravers@mybest.org.uk>
2021-03-05 15:54:57 +0000

    yes

--- commands/avatar.js
+++ commands/avatar.js
@@ -1,34 +1,34 @@
-/*module.exports = {
-  name: 'avatar',
-  description: "Get's a user's avatar",
-  execute(message, args, Client){
-    const matches = mention.match(/^<@!?(\d+)>$/);
+// module.exports = {
+//   name: 'avatar',
+//   description: "Get's a user's avatar",
+//   execute(message, args, Client){
+//     const matches = mention.match(/^<@!?(\d+)>$/);
 
-	// If supplied variable was not a mention, matches will be null instead of an array.
-	if (!matches) return;
+// 	// If supplied variable was not a mention, matches will be null instead of an array.
+// 	if (!matches) return;
 
-	// However the first element in the matches array will be the entire mention, not just the ID,
-	// so use index 1.
-	const id = matches[1];
+// 	// However the first element in the matches array will be the entire mention, not just the ID,
+// 	// so use index 1.
+// 	const id = matches[1];
 
-	return client.users.cache.get(id);
+// 	return client.users.cache.get(id);
   
   
       
-    if (args[1]) {
+//     if (args[1]) {
       
-      const user = getUserFromMention(args[1]);
+//       const user = getUserFromMention(args[1]);
 
-      if (!user) {
-          return message.reply('Please use a proper mention if you want to see someone else\'s avatar.');
-      }
+//       if (!user) {
+//           return message.reply('Please use a proper mention if you want to see someone else\'s avatar.');
+//       }
 
-      return message.channel.send(`${user.username}'s avatar: ${user.displayAvatarURL({ dynamic: true })}`);
-  }
+//       return message.channel.send(`${user.username}'s avatar: ${user.displayAvatarURL({ dynamic: true })}`);
+//   }
 
-  return message.channel.send(`${message.author.username}, your avatar: ${message.author.displayAvatarURL({ dynamic: true })}`);
+//   return message.channel.send(`${message.author.username}, your avatar: ${message.author.displayAvatarURL({ dynamic: true })}`);
 
 
-}
+// }
 
-}*/
+// }
--- index.js
+++ index.js
@@ -5,6 +5,7 @@ process.on('uncaughtException', function (err) {
 const Discord = require('discord.js');
 const { MessageEmbed } = require('discord.js');
 const Client = new Discord.Client({fetchAllMembers: true});
+const bot = new Discord.Client()
 const prefix = '>';
 
 
@@ -22,7 +23,9 @@ Client.ws.on('INTERACTION_CREATE', async interaction => {
   require(`./slash_commands/${interaction.data.name}.js`)(client, interaction);
 })
 const cooldowns = new Discord.Collection();
-
+bot.once('ready', () => {
+  console.log('Reaction Bot Online')
+})
 Client.once('ready', async () => {
   console.log('Online!');
   let guilds = await Client.guilds.cache
@@ -90,6 +93,115 @@ const activities_list = [
       require(`./slash_commands/${interaction.data.name}.js`)(client, interaction);
   })
 
+  bot.on('message', async (message) => {
+    
+    if(message.author.bot) return;
+    if(message.channel.id === '815681703192428574'){
+        if(message.attachments.size > 0) {
+            const Attachment = message.attachments.array()
+            const RateEmbed = new Discord.MessageEmbed()
+            .setColor('#ff000')
+            .setTitle('Rate')
+            .setImage(Attachment[0].url)
+            .setTimestamp()
+            message.channel.send(RateEmbed).then(async embed => {
+            message.delete()    
+                
+                await embed.react("<:1_:815696595521634314>")
+                await embed.react("<:2_:815696652911509504>")
+                await embed.react("<:3_:815696653137870848>")
+                await embed.react("<:4_:815696653746569217>")
+                await embed.react("<:5_:815696654673379388>")
+                await embed.react("<:6_:815696654174126110>")
+                await embed.react("<:7_:815696654187495424>")
+                await embed.react("<:8_:815696654299955230>")
+                await embed.react("<:9_:815696653826654270>")
+                await embed.react("<:10:815696654304149514>")
+                
+            }) .catch(() => console.error('One of the emojis failed to react.')); 
+                
+        } else if(message.attachments.size < 0){
+            return;
+        }
+    } else if(message.channel.id === '815684970064248842') {
+        if(message.attachments.size > 0){
+            const HotOrNotAttachment = message.attachments.array()
+            const HotOrNotEmbed = new Discord.MessageEmbed()
+            .setColor('#ff000')
+            .setTitle('Hot Or Not')
+            .setImage(HotOrNotAttachment[0].url)
+            .setTimestamp()
+            
+            message.channel.send(HotOrNotEmbed).then(async embed => {
+                message.delete()
+                await embed.react("<:hot:815698544622567494>")
+                await embed.react("<:not:815698544747872276>")
+            
+            }) .catch(() => console.error('One of the emojis failed to react.')); 
+
+        } else if(message.attachments.size < 0){
+            return;
+        }
+    } else if(message.channel.id === '815681738990026763') {
+        if(message.attachments.size > 0){
+            const LeftRightAttachment = message.attachments.array()
+            const LeftRightEmbed = new Discord.MessageEmbed()
+            .setColor('#ff000')
+            .setTitle('Left or Right')
+            .setImage(LeftRightAttachment[0].url)
+            .setTimestamp()
+            
+            message.channel.send(LeftRightEmbed).then(async embed => {
+                message.delete()
+       
+                await embed.react("<:leftarrow:815700275406176346>")
+                await embed.react("<:rightarrow:815700275074564129>")
+            }) .catch(() => console.error('One of the emojis failed to react.')); 
+
+        } else if(message.attachments.size < 0){
+            return;
+        }
+    } else if(message.channel.id === '815681377751793674') {
+        if(message.attachments.size > 0){
+            const BonerAttachment = message.attachments.array()
+            const BonerEmbed = new Discord.MessageEmbed()
+            .setColor('#ff000')
+            .setTitle('Boner-O-Meter')
+            .setImage(BonerAttachment[0].url)
+            .setTimestamp()
+            
+            message.channel.send(BonerEmbed).then(async embed => {
+                message.delete()
+                await embed.react("<:b3Limp:815702520768102420>")
+                await embed.react("<:b2Hard:815702520781340702>")
+                await embed.react("<:b1Rock:815702016453378058>")
+            
+            }) .catch(() => console.error('One of the emojis failed to react.')); 
+        } else if(message.attachments.size < 0){
+            return;
+        }
+
+    } else if(message.channel.id === '815681674074652723') {
+        if(message.attachments.size > 0){
+            const SmashOrPassAttachment = message.attachments.array()
+            const SmashOrPassEmbed = new Discord.MessageEmbed()
+            .setColor('#ff000')
+            .setTitle('Smash Or Pass')
+            .setImage(SmashOrPassAttachment[0].url)
+            .setTimestamp()
+            
+            message.channel.send(SmashOrPassEmbed).then(async embed => {
+                message.delete()
+                 await embed.react("<:samsh:815699131548434472>")
+                 await embed.react("<:pass:815699131075002370>")
+            }) .catch(() => console.error('One of the emojis failed to react.')); 
+
+        } else if(message.attachments.size < 0){
+            return;
+        }
+    }
+})
+
   Client.on('message', message => {
     if(message.guild.id == '729703207848378460'){
       const keywords = ["i dont simp over shawty","shawty like a melody"]
@@ -141,6 +253,7 @@ const activities_list = [
 
     
   });
+bot.login(process.env.REACTION_BOT_TOKEN)
 Client.login(process.env.DJS_TOKEN); //you should not use process.env
 
 
