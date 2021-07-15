472c9d960bf75accdd189735b6bd3cce3fac95e7
shyguy2601 <haydentravers2601@gmail.com>
2021-07-15 04:32:20 +0100

    Update teachrequest.js

--- teachcommands/teachrequest.js
+++ teachcommands/teachrequest.js
@@ -1,1 +1,46 @@
+module.exports = {
+    name: 'request',
+    description: "this is a request command",
+    aliases: ['r', 'rqst'],
+    execute(message, args){
+      const Discord = require('discord.js');
+      const { MessageEmbed } = require('discord.js')
+      
+      const featurerequest = args.slice(1).join(" ")
+    
 
+    const UserRequestEmbed = new Discord.MessageEmbed()
+    .setColor('#DC143C')
+    .setTitle('Request')
+    .setDescription(`Request from ${message.author} in server ${message.guild.name}. Their Request is \n
+    ${featurerequest}`)
+   
+    
+    const RequestWasSentEmbed = new Discord.MessageEmbed()
+    .setColor('#DC143C')
+    .setTitle('Success!')
+    .setDescription(`Your request has successfully been submitted!`)
+    .addField(`\u200B`,`Your request:- ${featurerequest}`)
+    .setFooter(`Request from ${message.author.username}`)
+
+    if(!featurerequest){
+        message.channel.send('You need to give a request!')
+        return;
+    }
+    message.client.users.fetch("704708159901663302").then(user=>{
+        user.send(UserRequestEmbed)
+        .then(embed => {
+          embed.react('✉️')
+          message.channel.send(RequestWasSentEmbed);
+        })
+      })
+    
+  
+    
+  
+    
+            
+        
+    } 
+  } 
+  
